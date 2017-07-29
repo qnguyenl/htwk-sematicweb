3dc70ffa32bcf025c1830fa2bed16ef8d2265917
Quy Nguyen Lam <hr.nguyenlq@gmail.com>
2017-07-29 04:54:19 +0200

    add result diagramm

--- rdf/query.py
+++ rdf/query.py
@@ -4,20 +4,23 @@ from rdflib import Graph
 from rdflib.plugins.sparql.results.csvresults import CSVResultSerializer
 
 g = Graph()
-g.parse("./data/result/data.rdf",format="xml")
-q = """SELECT ?stateName (COUNT(?car) AS ?ANZAHL) ?population
+g.parse("../data/result/data.rdf",format="xml")
+q = """SELECT ?stateName (COUNT(?car) AS ?ANZAHL) ?brandName
 		WHERE {
-			?car car:plz ?plz .
+			?car car:plz ?plz ;
+				car:brand ?brand .
+			?brand brand:name ?brandName .
 			?plz plz:state ?state .
-			?state state:name ?stateName ;
-					state:populationEachKm2 ?population .
+			?state state:name ?stateName .
+			FILTER regex(?stateName, "^Sachsen") 
 		}
-		GROUP BY ?state 
+		GROUP BY ?state ?brand
+		ORDER BY ?stateName DESC(?ANZAHL) LIMIT 5
 		"""
 rs = g.query(q)
-with open('./data/result/spraqlResult.csv','w') as csvfile:
+with open('../data/result/spraqlResult.csv','w') as csvfile:
 	write = csv.writer(csvfile, delimiter=',', quotechar='"')
-	write.writerow(['Bundesland','cars','Einwohnerzahl je Km2'])
+	write.writerow(['Bundesland','cars','Brand'])
 	for row in rs.result:
-		print('%s %s %s' % row)
+		#print('%s %s %s' % row)
 		write.writerow(row)
